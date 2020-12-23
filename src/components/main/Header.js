639445a8f1e5a0a8177bdfdb92bddc818518a763
ljj <leejj2002@naver.com>
2020-12-23 22:38:14 +0900

    header highlight position changed & smooth scroll for safari

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -5,35 +5,38 @@ import styled from "styled-components";
 const Header = ({ contentsRef }) => {
   const headerRef = useRef();
   const handleClick = (event) => {
-    const target = document.querySelector(event.target.hash);
-    window.scrollTo({
-      top:
-        target.offsetTop + window.innerHeight - headerRef.current.clientHeight,
-      behavior: "smooth",
-    });
     event.preventDefault();
+    const target = document.querySelector(event.target.hash);
+    const targetPosition =
+      target.offsetTop + window.innerHeight - headerRef.current.clientHeight;
+    for (let i = window.scrollY, timeSetter = 0; i !== targetPosition; ) {
+      targetPosition - window.scrollY > 0 ? i++ : i--;
+      setTimeout(() => window.scrollTo({ top: i }), timeSetter / 5);
+      timeSetter++;
+    }
   };
+
   const [scrollPosition, setScrollPosition] = useState(null);
   const handleHeaderHighlight = () => {
     const targetList = contentsRef.current.childNodes;
     if (
       window.scrollY >
-      targetList[3].offsetTop + targetList[3].clientHeight / 2
+      targetList[3].offsetTop + targetList[3].clientHeight * 0.7
     ) {
       setScrollPosition(3);
     } else if (
       window.scrollY >
-      targetList[2].offsetTop + targetList[2].clientHeight / 2
+      targetList[2].offsetTop + targetList[2].clientHeight * 0.7
     ) {
       setScrollPosition(2);
     } else if (
       window.scrollY >
-      targetList[1].offsetTop + targetList[1].clientHeight / 2
+      targetList[1].offsetTop + targetList[1].clientHeight * 0.7
     ) {
       setScrollPosition(1);
     } else if (
       window.scrollY >
-      targetList[0].offsetTop + targetList[0].clientHeight / 2
+      targetList[0].offsetTop + targetList[0].clientHeight * 0.7
     ) {
       setScrollPosition(0);
     } else {
